3c94a31f1a6e5ce00e82be61942c0c2ecb910cd1
bijith <bijithpn@gmail.com>
2024-10-28 11:36:57 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokemon TCG',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(
+        title: 'Pokemon TCG',
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  final String title;
+  const HomeScreen({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text("Home"),
+      ),
+    );
+  }
+}
